27caf7dad179f2e87d15d4f06ea851f0ea560c3a
Javier Evelyn <javier@appvida.co>
2016-08-01 13:48:04 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+        $("h1").css("background-color", "green");
+    });
+});
+
+var employee = {
+	name: "Javier",
+	age: 21,
+	married: 1,
+}
+
+// Selectors: Element, Class, or ID
